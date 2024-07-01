e654dad81f3a0092e33bf439e421e417f65dac1c
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 13:48:50 +0200

    removed staticfiles

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -139,9 +139,6 @@ APPEND_SLASH = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale/")]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

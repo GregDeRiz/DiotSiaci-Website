06aab4cdbbaada1d9fe19cbab7805d561315ddec
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 09:32:49 +0200

    Changed static location v2

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -141,11 +141,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 APPEND_SLASH = True
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 LOCALE_PATHS = [os.path.join(BASE_DIR, "locale/")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
